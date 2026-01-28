c7252a2e25f5fee3020fa16dc390d7645c9e01a2
Alejandro Cabrera <tv01d@Alejandros-MacBook-Air.local>
2026-01-28 08:48:47 -0500

    Use dynamic basename from Vite base URL for React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const basePath = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={basePath}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
